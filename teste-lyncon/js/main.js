9cf6e36662522691eecd441100c35fbef119b428
duartelyncon <79946618+duartelyncon@users.noreply.github.com>
2024-08-18 17:24:34 -0300

    Subindo o Projeto Completo
    Adiciona projeto completo de HTML e CSS para teste prático

---
+++ teste-lyncon/js/main.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+  $('#slider').nivoSlider({
+    effect: 'fade',
+    animSpeed: 500,
+    pauseTime: 3000, 
+    controlNav: false, 
+    pauseOnHover: true 
+  });
+});
+
+/* Nivo Slider, optei por realizar essas configurações */
+
+
